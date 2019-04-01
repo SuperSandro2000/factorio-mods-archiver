2f83cfd6a0a0e2f174c39d9e6d5e10657719b61e
Sandro Jäckel <sandro.jaeckel@gmail.com>
2019-04-02 00:15:06 +0200

    Convert .sha1 files to LF before checking hash

--- check-all-sha1.sh
+++ check-all-sha1.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+set -ex
+find -name "*.sha1" | xargs -I % dos2unix -q "%" > /dev/null
 find -name "*.sha1" | xargs -I % sh -c "cd \"\$(dirname \"%\")\" && sha1sum -c \"\$(basename \"%\")\" > /dev/null || echo fail %"
