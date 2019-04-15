940c32e410a7c715cea19883092bda8942563b5f
Sandro Jäckel <sandro.jaeckel@gmail.com>
2019-04-16 00:39:02 +0200

    Enable pipefail

--- check-all-sha1.sh
+++ check-all-sha1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -ex
-find -name "*.sha1" | xargs -I % dos2unix -q "%" > /dev/null
+set -exou pipefail
+find -name "*.sha1" | xargs -I % dos2unix -q "%" >/dev/null
 find -name "*.sha1" | xargs -I % sh -c "cd \"\$(dirname \"%\")\" && sha1sum -c \"./\$(basename \"%\")\" > /dev/null || echo fail %"
 echo "No errors found"
